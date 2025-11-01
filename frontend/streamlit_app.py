4d4ec59e2e0c96b73d57e0ba29c12f56df8ea396
fescobarzo <fescobarzo@bdp.com.py>
2025-11-01 15:50:17 -0300

    Added new pydeck graph

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 from io import StringIO
+import pydeck as pdk
 
 # CONFIG
 API_KEY = "cbae442f3a983932ea8938d9b2a76acc"  # Replace with your actual MAP_KEY
@@ -25,6 +26,29 @@ try:
     st.success("Data fetched successfully!")
     st.write("### Available Data by Country and Date")
     st.dataframe(df)
+    st.subheader("🔥 Fire Intensity Map (by Brightness)")
+
+    if not df.empty:
+        st.pydeck_chart(pdk.Deck(
+            map_style="mapbox://styles/mapbox/dark-v10",
+            initial_view_state=pdk.ViewState(
+                latitude=df["latitude"].mean(),
+                longitude=df["longitude"].mean(),
+                zoom=5,
+                pitch=40,
+            ),
+            layers=[
+                pdk.Layer(
+                    "ScatterplotLayer",
+                    data=df,
+                    get_position='[longitude, latitude]',
+                    get_fill_color='[255, brightness - 250, 0, 160]',
+                    get_radius=10000,
+                    pickable=True,
+                ),
+            ],
+        ))
+
 
 except requests.HTTPError as err:
     st.error(f"HTTP Error: {err}")
