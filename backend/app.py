4fe1112ca38e1972028f7b4bcb4c0e87502fd17c
fescobarzo <fescobarzo@bdp.com.py>
2025-11-08 14:27:44 -0300

    fixed backend

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,7 @@ def get_fires():
 
         return jsonify(df.to_dict(orient="records"))
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify([])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
